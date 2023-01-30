99cf0b5c98a53cce2f994466685983a0112880a9
Denis <adadenis1982@yandex.ru>
2023-01-30 10:12:22 +0300

    done

--- src/pages/events/index.js
+++ src/pages/events/index.js
@@ -1,3 +1,17 @@
-export default function Page() {
-  return <h1>First Events</h1>;
+import AllEvents from '../../components/events/events-page';
+
+const EventsPage = ({ data }) => {
+  return <AllEvents data={data} />;
+};
+
+export default EventsPage;
+
+export async function getStaticProps() {
+  const { events_categories } = await import('../../data/data.json');
+
+  return {
+    props: {
+      data: events_categories,
+    },
+  };
 }
